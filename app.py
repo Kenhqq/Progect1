eae203ef12d8383859ad455cd47241d87ef3678c
MWildFire <mvstrike17@gmail.com>
2021-02-06 16:46:36 +0300

    added Flask-Mail

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_mail import Mail, Message
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'social_media.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.update(
+    DEBUG=True,
+    # Настройки почты
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=465,
+    MAIL_USERNAME='mvstrike17@gmail.com',
+    MAIL_PASSWORD='',
+    MAIL_USE_SSL=True
+)
+
+mail = Mail(app)
+db = SQLAlchemy(app)
 
 app = Flask(__name__)
 
